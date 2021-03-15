5022455e62514d86e87c7d71ed5fdf7f9c585013
Md Irfan <31414222+mdirfan121@users.noreply.github.com>
2021-03-15 17:00:58 +0530

    Create Diffk.java

---
+++ Diffk.java
@@ -0,0 +1,13 @@
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int diffPossible(final int[] A, int B) {
+        Set<Integer> set=new HashSet<Integer>();
+        for(int i=0;i<A.length;i++){
+            if(set.contains(A[i]-B) || set.contains(A[i]+B)){
+                return 1;
+            }
+            else set.add(A[i]);
+        }
+        return 0;
+    }
+}
