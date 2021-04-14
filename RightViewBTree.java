37734dc388a29b439fec6710c3c6dbd37ad09d69
Md Irfan <31414222+mdirfan121@users.noreply.github.com>
2021-04-14 11:26:38 +0530

    Create RightViewBTree.java

---
+++ RightViewBTree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> solve(TreeNode A) {
+        ArrayList<Integer> level=new ArrayList<Integer>();
+        if(A==null) return level;
+        int tempans=-1;
+        Queue<TreeNode> queue=new LinkedList<TreeNode>();
+        Stack<TreeNode> stk=new Stack<TreeNode>();
+        queue.add(A);
+        queue.add(null);
+        while(!queue.isEmpty()){
+            TreeNode temp= queue.remove();
+            if(temp!=null){
+                tempans=temp.val;
+                if(temp.left!=null){
+                    queue.add(temp.left);
+                }
+                if(temp.right!=null){
+                    queue.add(temp.right);
+                }
+            }
+            else{
+                level.add(tempans);
+                if(queue.isEmpty()){
+                    break;
+                }
+                queue.add(temp);
+            }
+        }
+        return level;
+    }
+}
