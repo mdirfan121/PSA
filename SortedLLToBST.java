d0e2e2d0c7bb5c976b224959d0605b90bbff7228
Md Irfan <31414222+mdirfan121@users.noreply.github.com>
2022-01-08 19:47:29 +0530

    Create SortedLLToBST.java

---
+++ SortedLLToBST.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        return sortedBST(head , null);
+    }
+    public static TreeNode sortedBST(ListNode start, ListNode end){
+        if(start == end) return null;
+        ListNode slow = start;
+        ListNode fast = start;
+        while(fast!=end && fast.next!=end){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sortedBST(start,slow);
+        root.right = sortedBST(slow.next, end);
+    
+        return root;
+    }
+}
