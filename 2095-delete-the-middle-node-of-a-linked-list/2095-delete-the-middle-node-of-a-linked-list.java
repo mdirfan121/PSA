b28e5688b53943aaeb28b4ad73dfa31895de022a
Md Irfan <31414222+mdirfan121@users.noreply.github.com>
2022-02-09 14:20:01 +0530

    Time: 5 ms (64.34%), Space: 243.7 MB (5.01%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -10,46 +10,56 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        ListNode main;
-        if(count(head)==1) return null;
-        if(count(head)%2==1){
-            main = oddMiddle(head);
+        if(head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null&&fast.next.next!=null&&fast.next.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        else{
-            main = evenMiddle(head);
-        }
-        main.next = main.next.next;
+        slow.next = slow.next.next;
         return head;
+    }
+//         ListNode main;
+//         if(count(head)==1) return null;
+//         if(count(head)%2==1){
+//             main = oddMiddle(head);
+//         }
+//         else{
+//             main = evenMiddle(head);
+//         }
+//         main.next = main.next.next;
+//         return head;
         
-    }
-    public static ListNode evenMiddle(ListNode temp){
-        if(temp==null) return temp;
-        ListNode slow = temp;
-        ListNode fast = temp;
-        while(fast.next != null && fast.next.next != null){
-          slow = slow.next;
-          fast = fast.next.next;  
-        }
-        return slow;
-    }
-    public static ListNode oddMiddle(ListNode temp){
-        if(temp==null) return temp;
-        ListNode slow = temp;
-        ListNode fast = temp;
-        while(slow.next!=null && fast.next.next!=null && fast.next.next.next!=null){
-          slow = slow.next;
-          fast = fast.next.next;  
-        }
-        return slow;
-    }
-    public static int count(ListNode n){
-        if(n==null) return 0;
-        ListNode temp = n;
-        int count = 0;
-        while(temp!=null){
-            count+=1;
-            temp = temp.next;
-        }
-        return count;
-    }
+//     }
+//     public static ListNode evenMiddle(ListNode temp){
+//         if(temp==null) return temp;
+//         ListNode slow = temp;
+//         ListNode fast = temp;
+//         while(fast.next != null && fast.next.next != null){
+//           slow = slow.next;
+//           fast = fast.next.next;  
+//         }
+//         return slow;
+//     }
+//     public static ListNode oddMiddle(ListNode temp){
+//         if(temp==null) return temp;
+//         ListNode slow = temp;
+//         ListNode fast = temp;
+//         while(slow.next!=null && fast.next.next!=null && fast.next.next.next!=null){
+//           slow = slow.next;
+//           fast = fast.next.next;  
+//         }
+//         return slow;
+//     }
+//     public static int count(ListNode n){
+//         if(n==null) return 0;
+//         ListNode temp = n;
+//         int count = 0;
+//         while(temp!=null){
+//             count+=1;
+//             temp = temp.next;
+//         }
+//         return count;
+//     }
 }
