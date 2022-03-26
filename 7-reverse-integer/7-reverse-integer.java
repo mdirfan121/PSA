dbc9727131438e4f13a6e11de1448756e86ce0bb
Md Irfan <31414222+mdirfan121@users.noreply.github.com>
2022-03-27 00:28:38 +0530

    Time: 1 ms (98.79%), Space: 42 MB (17.83%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        int flag = 0;
+        if(x<0){
+            flag = 1;
+        }
+        int temp = Math.abs(x);
+        long ans = 0;
+        while(temp>0){
+            int local = temp%10;
+            temp = temp/10;
+            ans = ans*10+local;
+        }
+        if(ans>Integer.MAX_VALUE) return 0;
+        else if(ans<Integer.MIN_VALUE) return 0;
+        else{
+            if(flag ==1) return (int)(-ans);
+            else return (int)ans;
+        }
+    }
+}
